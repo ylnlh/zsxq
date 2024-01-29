abe76c30c653f5b2690e30eec8d234b4d7054e46
yangle <yanglenlh@163.com>
2024-01-29 13:31:04 +0800

    切换基础路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,8 +4,8 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  // base: "/zsxq/", // 上传路径
-  base: "/", // 本地路径，因为我不是根目录
+  base: "/zsxq/", // 上传路径
+  // base: "/", // 本地路径，因为我不是根目录
 
 
   lang: "zh-CN",
